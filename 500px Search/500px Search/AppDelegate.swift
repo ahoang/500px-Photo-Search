e1c10309fafdb204bdcb2c2fb3f82a86671b100d
Anthony Hoang <anthony.hoang@slalom.com>
2017-11-28 12:35:18 -0600

    created project

---
+++ 500px Search/500px Search/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  500px Search
+//
+//  Created by Anthony Hoang on 11/28/17.
+//  Copyright © 2017 Anthony Hoang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 500px Search/500px Search/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  500px Search
+//
+//  Created by Anthony Hoang on 11/28/17.
+//  Copyright © 2017 Anthony Hoang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
